34a740762f3bd98c0c0463e223b11ee596f79774
Adam Munaverali <adammunaverali@gmail.com>
2024-06-06 16:09:45 +0800

    nav bar completed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import logo from '../images/Logo .svg'
 const Nav = () => {
+    const [menuOpen, setMenuOpen] = React.useState(false)
+    const toggleMenu = () =>{
+        setMenuOpen(!menuOpen);
+    }
     return (
-        <nav>
-            <a href='/'>
+        <nav className={`navbar ${menuOpen ? "open" :""}`}>
+            <a href='/' className='logo'>
                 <img src={logo} alt = 'logo'/>
             </a>
 
-            <div>
+            <div className='menu-icon' onClick={toggleMenu}>
                 <div className='bar'></div>
                 <div className='bar'></div>
                 <div className='bar'></div>
             </div>
 
-            <ul>
+            <ul className={`nav-links ${menuOpen ? "visible" :""}`}>
                 <li>
                     <a href='/'>Home</a>
                 </li>
